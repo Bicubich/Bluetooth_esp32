77e56182930c38dff828f04cfd7aac4323aada23
Bicubich <98553134+Bicubich@users.noreply.github.com>
2023-02-14 01:53:34 +0300

    ver 4.0

--- Bluetooth_app/app/src/main/java/com/example/bluetooth_esp32/ControlActivity.kt
+++ Bluetooth_app/app/src/main/java/com/example/bluetooth_esp32/ControlActivity.kt
@@ -1,17 +1,21 @@
 package com.example.bluetooth_esp32
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bluetooth_esp32.databinding.ActivityControlBinding
-import java.io.IOException
 
 class ControlActivity : AppCompatActivity() {
     private lateinit var binding: ActivityControlBinding
@@ -19,14 +23,29 @@ class ControlActivity : AppCompatActivity() {
     lateinit var btConnection: BtConnection
     private var listItem: ListItem? = null
 
+    private var spinnerAdapter: ArrayAdapter<String>? = null
+
+    private var pref: SharedPreferences? = null
+    var commands: Array<String> = arrayOf()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityControlBinding.inflate(layoutInflater)
         setContentView(binding.root)
         onBtListResult()
         init()
+        initMemoryData()
+        initSpinner()
         binding.bSendMessage.setOnClickListener{
-            btConnection.sendMessage(binding.etMessage.text.toString())
+            var msg = binding.etMessage.text.toString()
+            // we save the command when it is not in the last 10 in memory
+            if (!isContainsComand(msg))
+                saveData(msg)
+            btConnection.sendMessage(msg)
+            binding.tvChatHostory.text.toString().contains("\n")
+            binding.tvChatHostory.text = binding.tvChatHostory.text.toString() + "\nUser: $msg"
+            binding.etMessage.setText("")
         }
     }
 
@@ -34,8 +53,9 @@ class ControlActivity : AppCompatActivity() {
         val btManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         val btAdapter = btManager.adapter
         btConnection = BtConnection(btAdapter)
-        binding.bSendMessage.isEnabled = false
-        binding.etMessage.isEnabled = false
+        //binding.bSendMessage.isEnabled = false
+        //binding.etMessage.isEnabled = false
+        pref = getSharedPreferences("TABLE", Context.MODE_PRIVATE)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -77,5 +97,114 @@ class ControlActivity : AppCompatActivity() {
                 binding.tvCurrentDevice.text =  "Current device: " + listItem!!.name
             }
         }
+    }
+
+    // saving commands in the phone memory
+    private fun saveData(comm: String){
+        replaceData()
+        val editor = pref?.edit()
+        editor?.putString("C1", comm)
+        editor?.apply()
+
+        val keys: Map<String, *> = pref?.all as Map<String, *>
+        for ((key, value) in keys) {
+
+            Log.d(
+                "MyLog", key + ": " +
+                        value.toString()
+            )
+
+        }
+
+        commands = getSpinnerData()
+        initSpinner()
+
+        editor?.apply()
+    }
+
+    // function initializes keys in phone memory
+    private fun initMemoryData(){
+        // check memory sheet
+        if (!pref?.contains("C1")!!) {
+            val editor = pref?.edit()
+            editor?.putString("C1", "")
+            editor?.putString("C2", "")
+            editor?.putString("C3", "")
+            editor?.putString("C4", "")
+            editor?.putString("C5", "")
+            editor?.putString("C6", "")
+            editor?.putString("C7", "")
+            editor?.putString("C8", "")
+            editor?.putString("C9", "")
+            editor?.putString("C10", "")
+            editor?.apply()
+        }
+        else{
+            commands = getSpinnerData()
+        }
+    }
+
+    // function moves the elements forward by one position
+    private fun replaceData(){
+        var bufLastCommand = ""
+        var bufNextCommand = ""
+
+        val memoryCell = MemoryCell()
+
+        val editor = pref?.edit()
+        for (mCell in memoryCell.listSheets) {
+            bufLastCommand = pref?.getString(mCell, "").toString()
+            editor?.putString(mCell, bufNextCommand)
+            bufNextCommand = bufLastCommand
+        }
+        editor?.apply()
+    }
+
+    // function check repeat commands in spinner
+    fun isContainsComand(comm: String): Boolean{
+        val keys: Map<String, *> = pref?.all as Map<String, *>
+        for ((key, value) in keys) {
+            if (comm == value.toString())
+                return true
+        }
+        return false
+    }
+
+    // function return actual saved commands to spinner's data array
+    private fun getSpinnerData(): Array<String> {
+        val listCommand: MutableList<String> = mutableListOf()
+        val memoryCell = MemoryCell()
+        var value = ""
+        for (mCell in memoryCell.listSheets) {
+            value = pref?.getString(mCell, "").toString()
+            if (value != "")
+                listCommand.add(value.toString())
+        }
+
+        commands = listCommand.toTypedArray()
+
+        return commands
+    }
+
+    // initialization spinner
+    private fun initSpinner() {
+        spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, commands)
+        binding.spinner.adapter = spinnerAdapter
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                binding.etMessage.setText(commands[position].toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
 }
---
+++ Bluetooth_app/app/src/main/java/com/example/bluetooth_esp32/MemoryCell.kt
@@ -0,0 +1,16 @@
+package com.example.bluetooth_esp32
+
+data class MemoryCell(
+    var listSheets: List<String> = listOf<String>(
+        "C1",
+        "C2",
+        "C3",
+        "C4",
+        "C5",
+        "C6",
+        "C7",
+        "C8",
+        "C9",
+        "C10"
+    )
+)
